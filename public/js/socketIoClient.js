608597fcf1a564ac74665ac7d5a33e549af81b5a
Nick Zorych <neo.vsmu@gmail.com>
2017-10-08 17:40:33 +0300

    hotfix locations

--- public/js/socketIoClient.js
+++ public/js/socketIoClient.js
@@ -3,7 +3,7 @@
  *
  * This js script subscribe to channel "newChatMessage" and if meggage received update message view
  */
-const socket = io('http://192.168.10.10:3000');
+const socket = io(window.location.protocol+'//'+window.location.hostname+':3000');
 socket.on('newChatMessage',function (data) {
     var messageObject = JSON.parse(data)
     $('#message').append('<li>'+messageObject.time+' '+messageObject.user+' '+messageObject.message)
